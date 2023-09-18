07c35a0826986b0abd08257f32a991ef65010bc1
Edenbdd <eden.bertrandd@gmail.com>
2023-09-18 11:18:44 +0200

    ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,12 +1,42 @@
 /*42 header*/
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+#include"libft.h"
+
+t_list  *ft_lstnew(void *content)
 {
-	del(lst);
+        t_list *head;
+
+        head = malloc(sizeof(t_list) * 1);
+	if (!head)
+		return (NULL);
+        head->content = content;
+	head->next = NULL;
+        return(head);
+}
+
+void	*del(void *lst)
+{
+	free(lst);
+}
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{	
+	del(lst->content);
+	free(lst);
 }
 
 int	main(void)
 {
+	int	*ptr = malloc(sizeof(int));
+	*ptr = 4;
+	t_list *head = ft_lstnew((void *)ptr);
+	void	*ptr_del;	
 
+	ptr_del = &del;
+	printf("%ls\n", (int *)head->content);
+	printf("%p\n", head->next);
+	ft_lstdelone(head, ptr_del);
+	printf("%ls\n", (int *)head->content);
+	printf("%p\n", head->next);
 	return (0);
 }
